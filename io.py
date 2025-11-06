40efd133447af897a40cd32df999137532d08942
Gabriel Pospischil <gabriel-pospischil@t-online.de>
2025-11-06 13:26:02 +0100

    finished step 3

---
+++ io.py
@@ -0,0 +1,13 @@
+with open("wiki.txt", encoding='utf-8') as f:
+    wikiList = []
+    for line in f:
+        wikiList.append(line)
+    shortList = [x for x in wikiList if len(x) < 30]
+    articleList = [x for x in wikiList if x.startswith(("Der", "Die", "Das")) ]
+    aprilList = [x for x in wikiList if "April" in x]
+    with open("short.txt", "w", encoding="utf-8") as shortFile:
+        shortFile.writelines(shortList)
+    with open("article.txt", "w", encoding="utf-8") as artFile:
+        artFile.writelines(articleList)
+    with open("april.txt", "w", encoding="utf-8") as aprFile:
+        aprFile.writelines(aprilList)
